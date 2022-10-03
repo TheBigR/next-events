ffa66606a15a134a3bd345d7d184c60beb05ed6c
TheBigR <nevoroy@gmail.com>
2022-10-03 15:31:51 +0300

    event details created

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,33 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 function EventsDetailsPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No Event Found!</p>;
+  }
+
   return (
-    <div>
-      <p>This is the events details page</p>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
